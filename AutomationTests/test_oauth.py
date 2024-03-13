e38a601048e5e3824f915afecb495076125af766
pallavi_wabale <pallavi.jadhav2008@gmail.com>
2024-03-13 15:05:41 +0100

    updated batch file

--- AutomationTests/test_oauth.py
+++ AutomationTests/test_oauth.py
@@ -7,5 +7,5 @@ def test_oauthtest():
     tokenurl="http://thetestingworldapi.com/Token"
     data={'grant_type':'password','username':'pj222','password':'Shreeram@24'}
     response=requests.post(tokenurl,data)
-    res=requests.get(url, auth=HTTPBasicAuth("pj222", "Shreeram@24"))
+    res=requests.get(tokenurl, auth=HTTPBasicAuth("pj222", "Shreeram@24"))
     print(res.text)
